0167104c4ec8e4e5c475f53b5cce04ca6603cc2a
Will Tinney <willtinney1@gmail.com>
2016-11-10 10:33:47 +0000

    Set up basic scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'cocktails#index'
+  resources :doses
+  resources :cocktails
+  resources :ingredients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
